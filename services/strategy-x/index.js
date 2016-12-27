9d5b76263eaa8c7b5adc998b200445dac809d8dd
Stefan Walther <Stefan.Walther@qlik.com>
2016-12-28 00:51:43 +0100

    ci: add services to docker-compose

--- services/strategy-x/index.js
+++ services/strategy-x/index.js
@@ -7,7 +7,16 @@ open.then(conn => conn.createChannel())
   .then(ch => ch.assertQueue(queue)
     .then(() => ch.consume(queue, msg => {
       if (msg !== null) {
+        console.log('message: ', msg);
         console.log('Got message from MQ: ', JSON.parse(msg.content));
+
+        // Do the long running job
+
+        // Log the result
+
+        // Mark completed in jobs-service
+
         ch.ack(msg);
       }
-    }))).catch(console.warn);
+    })))
+  .catch(console.warn);
